4db22c84911805a37c0733873d5c32aacb30c377
Eric Hodges <ehodges2004@gmail.com>
2018-05-23 14:58:30 -0700

    updated Hoek

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -13,7 +13,7 @@
         <div class="navbar-end">
           <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
           <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
-          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
+          <!-- <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link> -->
           <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
         </div>
       </div>
